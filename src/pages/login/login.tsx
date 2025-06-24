a882cc4c0da5a0caf2085f9a5abc7f758a980296
YashQodic <yash@qodictechnosoft.com>
2025-06-24 15:17:32 +0530

    Updated invoice pal android app icon

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,31 +1,44 @@
-import { IonButton,  IonIcon, IonImg, IonPage, IonText } from "@ionic/react";
+import { IonButton, IonIcon, IonImg, IonModal, IonPage, IonText } from "@ionic/react";
 import "../../common.css";
 import "./login.css"
 import introImg from "../../assets/images/loginTransparentSvg.svg"
 import logo from "../../assets/images/logo.png"
-import { arrowForwardCircle} from "ionicons/icons"
-const Login:React.FC = () => {
+import { arrowForwardCircle } from "ionicons/icons"
+import { useState } from "react";
+const Login: React.FC = () => {
+    const [showModal, setShowModal] = useState(false);
     return (
-        <IonPage className="login-container login-background-color">
-            <div className="login-logo-container">
-                <IonImg src={logo} className="login-logo"/>
-            </div>
-            <div className="login-item login-background-color">
-                <div className="login-logo-container">
-                    <IonImg src={introImg} className="login-logo"/>
+        <>
+            <IonPage className="login-container login-background-color">
+                <div className="login-item login-background-color">
+                    <div className="login-logo-container">
+                        <IonImg src={introImg} className="login-logo" />
+                    </div>
+                    <div className="login-subtitle">
+                        <IonText className="login-subtitle">Let’s scan your Gmail for invoices.</IonText>
+                    </div>
+                    <div className="login-button-container">
+                        <IonButton onClick={() => {
+                            setShowModal(true)
+                        }} expand="block" className="login-button">
+                            Get Started
+                            <IonIcon icon={arrowForwardCircle} slot="end" color="white" size="medium"></IonIcon>
+                        </IonButton>
+                    </div>
                 </div>
-                <div className="login-subtitle">
-                    <IonText className="login-subtitle">Let’s scan your Gmail for invoices.</IonText>
+            </IonPage>
+            <IonModal
+                isOpen={showModal}
+                onDidDismiss={() => setShowModal(false)}
+                breakpoints={[0, 0.5, 0.8]}
+                initialBreakpoint={0.3}
+                handleBehavior="cycle"
+            >
+                <div style={{ padding: 20 }}>
+                    <h2 className="login-modal-title">Login</h2>
                 </div>
-                <div className="login-button-container">
-                    <IonButton expand="block" className="login-button">
-                        Get Started
-                        <IonIcon icon={arrowForwardCircle} slot="end" color="white" size="medium"></IonIcon>
-                    </IonButton>
-                </div>
-            </div>
-        </IonPage>
-
+            </IonModal>
+        </>
     )
 }
 
