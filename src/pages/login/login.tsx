250792bbc5d5512917441c35213180c92ef49c8c
YashQodic <yash@qodictechnosoft.com>
2025-06-24 13:46:38 +0530

    Updated intro page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,10 +2,14 @@ import { IonButton,  IonIcon, IonImg, IonPage, IonText } from "@ionic/react";
 import "../../common.css";
 import "./login.css"
 import introImg from "../../assets/images/loginTransparentSvg.svg"
+import logo from "../../assets/images/logo.png"
 import { arrowForwardCircle} from "ionicons/icons"
 const Login:React.FC = () => {
     return (
         <IonPage className="login-container login-background-color">
+            <div className="login-logo-container">
+                <IonImg src={logo} className="login-logo"/>
+            </div>
             <div className="login-item login-background-color">
                 <div className="login-logo-container">
                     <IonImg src={introImg} className="login-logo"/>
